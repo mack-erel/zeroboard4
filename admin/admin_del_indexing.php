473ba5fda62d1dd1535f9ebbd2388fd0a9929e81
Henry <henry@Mac-mini.local>
2020-09-05 18:59:39 +0900

    tab 간격 맞춤

--- admin/admin_del_indexing.php
+++ admin/admin_del_indexing.php
@@ -1,44 +1,44 @@
 <?php
-  $_zb_path="../";
+$_zb_path = "../";
 
-  include_once "../lib.php";
-  include_once "../xmlrpc.inc.php";
-  include_once "../search.inc.php";
+include_once "../lib.php";
+include_once "../xmlrpc.inc.php";
+include_once "../search.inc.php";
 
-  $connect=dbconn();
+$connect = dbconn();
 
-  $member=member_info();
+$member = member_info();
 
-  if(!$member[no]||$member[is_admin]>1||$member[level]>1) Error("최고 관리자만이 사용할수 있습니다");
+if (!$member["no"] || $member["is_admin"] > 1 || $member["level"] > 1) Error("최고 관리자만이 사용할수 있습니다");
 
-  if($no) {
+if ($no) {
 
-    $query = "select * from zetyx_indexing where no = '{$no}'";
-    $data = mysql_fetch_array(mysql_query($query));
+	$query = "select * from zetyx_indexing where no = '{$no}'";
+	$data = mysql_fetch_array(mysql_query($query));
 
-    $oSearch = new zSearch();
-    $url = $_SERVER['HTTP_HOST'].str_replace("/admin/admin_del_indexing.php","",$_SERVER["PHP_SELF"]);
-    $obj->url = $url;
-    $obj->board_id = $data['table_id'];
-    $oSearch->delBoard($obj);
-    if($oSearch->return_code!=0) {
+	$oSearch = new zSearch();
+	$url = $_SERVER['HTTP_HOST'] . str_replace("/admin/admin_del_indexing.php", "", $_SERVER["PHP_SELF"]);
+	$obj->url = $url;
+	$obj->board_id = $data['table_id'];
+	$oSearch->delBoard($obj);
+	if ($oSearch->return_code != 0) {
 ?>
-<script>
-  alert("검색서버와의 연결을 실패하였습니다");
-  self.close();
-</script>
+		<script>
+			alert("검색서버와의 연결을 실패하였습니다");
+			self.close();
+		</script>
+	<?php
+	} else {
+		$query = "delete from zetyx_indexing where no = '{$no}'";
+		mysql_query($query);
+	?>
+		<script>
+			try {
+				opener.history.go(0);
+			} catch (e) {}
+			self.close();
+		</script>
 <?php
-    } else {
-      $query = "delete from zetyx_indexing where no = '{$no}'";
-      mysql_query($query);
-?>
-<script>
-  try{
-    opener.history.go(0);
-  } catch(e){}
-  self.close();
-</script>
-<?php
-    }
-  }
+	}
+}
 ?>
--- admin/admin_indexing.php
+++ admin/admin_indexing.php
@@ -1,6 +1,6 @@
 <?php
-  if(!isTable("zetyx_indexing")) {
-    $query = "
+if (!isTable("zetyx_indexing")) {
+	$query = "
       create table zetyx_indexing (
          no int(10) not null auto_increment primary key ,
          table_id char(40) not null,
@@ -14,70 +14,81 @@
          dw_board_id int(10) not null
       )
     ";
-    mysql_query($query);
-  }
+	mysql_query($query);
+}
 
-  $result = mysql_query("select * from zetyx_indexing order by table_id");
-  while($tmp = mysql_fetch_array($result)) {
-    $indexing_list[] = $tmp;
-  }
+$result = mysql_query("select * from zetyx_indexing order by table_id");
+while ($tmp = mysql_fetch_array($result)) {
+	$indexing_list[] = $tmp;
+}
 ?>
 <table border=0 cellspacing=0 cellpadding=10 bgcolor=999999 width=100% height=100%>
-<form name=showdb>
-<tr>
-	<td valign=top>
-	<br>
-	<table border=0 width=100%>
-	<col width=50%></col><col width=50%></col>
-	<tr>
-		<td nowrap><font color=white size=4 face=tahoma><b>Zeroboard & DreamWiz 검색서버 관리</b></font></td>
-	</tr>
-	</table>
-	<br>
+	<form name=showdb>
+		<tr>
+			<td valign=top>
+				<br>
+				<table border=0 width=100%>
+					<col width=50%>
+					</col>
+					<col width=50%>
+					</col>
+					<tr>
+						<td nowrap>
+							<font color=white size=4 face=tahoma><b>Zeroboard & DreamWiz 검색서버 관리</b></font>
+						</td>
+					</tr>
+				</table>
+				<br>
 
-	<table border=0 cellspacing=1 cellpadding=2 width=100% bgcolor=999999>
-    <col width=40></col>
-    <col width=*></col>
-    <col width=90></col>
-    <col width=85></col>
-    <col width=80></col>
-    <col width=40></col>
-	<tr bgcolor=444444 align=center>
-		<td style=color:white;font-size:8pt;font-family:tahoma>No</td>
-		<td style=color:white;font-size:8pt;font-family:tahoma>게시판 ID</td>
-		<td style=color:white;font-size:8pt;font-family:tahoma>검색률</td>
-		<td style=color:white;font-size:8pt;font-family:tahoma>indexing 단위</td>
-		<td style=color:white;font-size:8pt;font-family:tahoma>예약 시간</td>
-		<td style=color:white;font-size:8pt;font-family:tahoma>삭제</td>
-	</tr>
-<?php
-  for($i=0;$i<count($indexing_list);$i++) {
-?>
-  <tr bgcolor=#FFFFFF align=center height=25>
-    <td><?=($i+1)?></td>
-    <td><?=$indexing_list[$i]['table_id']?></td>
-    <td><?=sprintf("%0.2f",$indexing_list[$i]['point']/$indexing_list[$i]['stop_point']*100)?>%</td>
-    <td><?=$indexing_list[$i]['max_crawling']?>개</td>
-    <td>
-      <?php if(!$indexing_list[$i]['reserve_stime']&&!$indexing_list[$i]['reserve_etime']){?>
-      없음
-      <?php }else{?>
-      <?=sprintf("%02d",$indexing_list[$i]['reserve_stime'])?>시 ~
-      <?=sprintf("%02d",$indexing_list[$i]['reserve_etime'])?>시
-      <?php }?>
-    </td>
-    <td><a href="javascript:void(window.open('./admin/admin_del_indexing.php?no=<?=$indexing_list[$i]['no']?>','_DelBoard','width=400,height=200,noresizable,noscroll'))" onClick="return confirm('검색서버에서 삭제하시겠습니까?')">삭제</a></td>
-  </tr>
-<?php
-  }
-?>
-	</table>
+				<table border=0 cellspacing=1 cellpadding=2 width=100% bgcolor=999999>
+					<col width=40>
+					</col>
+					<col width=*>
+					</col>
+					<col width=90>
+					</col>
+					<col width=85>
+					</col>
+					<col width=80>
+					</col>
+					<col width=40>
+					</col>
+					<tr bgcolor=444444 align=center>
+						<td style=color:white;font-size:8pt;font-family:tahoma>No</td>
+						<td style=color:white;font-size:8pt;font-family:tahoma>게시판 ID</td>
+						<td style=color:white;font-size:8pt;font-family:tahoma>검색률</td>
+						<td style=color:white;font-size:8pt;font-family:tahoma>indexing 단위</td>
+						<td style=color:white;font-size:8pt;font-family:tahoma>예약 시간</td>
+						<td style=color:white;font-size:8pt;font-family:tahoma>삭제</td>
+					</tr>
+					<?php
+					for ($i = 0; $i < count($indexing_list); $i++) {
+					?>
+						<tr bgcolor=#FFFFFF align=center height=25>
+							<td><?= ($i + 1) ?></td>
+							<td><?= $indexing_list[$i]['table_id'] ?></td>
+							<td><?= sprintf("%0.2f", $indexing_list[$i]['point'] / $indexing_list[$i]['stop_point'] * 100) ?>%</td>
+							<td><?= $indexing_list[$i]['max_crawling'] ?>개</td>
+							<td>
+								<?php if (!$indexing_list[$i]['reserve_stime'] && !$indexing_list[$i]['reserve_etime']) { ?>
+									없음
+								<?php } else { ?>
+									<?= sprintf("%02d", $indexing_list[$i]['reserve_stime']) ?>시 ~
+									<?= sprintf("%02d", $indexing_list[$i]['reserve_etime']) ?>시
+								<?php } ?>
+							</td>
+							<td><a href="javascript:void(window.open('./admin/admin_del_indexing.php?no=<?= $indexing_list[$i]['no'] ?>','_DelBoard','width=400,height=200,noresizable,noscroll'))" onClick="return confirm('검색서버에서 삭제하시겠습니까?')">삭제</a></td>
+						</tr>
+					<?php
+					}
+					?>
+				</table>
 
-    <div align=right>
-      <input type=button value='게시판 추가' style=border-color:#b0b0b0;background-color:#3d3d3d;color:#ffffff;font-size:8pt;font-family:Tahoma;height:20px; onClick="window.open('./admin/admin_add_indexing.php','_AddIndexBoard','width=500,height=200,noresizable,noscroll')">
-    </div>
+				<div align=right>
+					<input type=button value='게시판 추가' style=border-color:#b0b0b0;background-color:#3d3d3d;color:#ffffff;font-size:8pt;font-family:Tahoma;height:20px; onClick="window.open('./admin/admin_add_indexing.php','_AddIndexBoard','width=500,height=200,noresizable,noscroll')">
+				</div>
 
-	</td>
-</tr>
-</form>
+			</td>
+		</tr>
+	</form>
 </table>
